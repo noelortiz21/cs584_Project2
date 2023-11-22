3896f4d2cce4c7f6e60ad2f943f99b4c5f759590
marvinh-csun <marvin.harootoonyan@csun.edu>
2023-11-21 19:21:46 -0800

    Fix Cors

--- tempo/app.py
+++ tempo/app.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 def create_app():
     from flask_json import FlaskJSON
+    from flask_cors import Cors
     from api.routes  import api_blueprint
     app = Flask(__name__,template_folder='templates')
     app.register_blueprint(api_blueprint, url_prefix="/api")
     app.config['UPLOAD_FOLDER'] = '/tmp'
     json = FlaskJSON()
     json.init_app(app)
+    CORS(app)
     return app
 
 if __name__ == "__main__":
